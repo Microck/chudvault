fd5f8e32920ec1fc1dfa08f1ca70422e2277cec0
Microck <45483921+Microck@users.noreply.github.com>
2026-01-22 01:46:53 +0100

    chore: remove mock data and reset db for launch

--- TweetVault/mock_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO bookmarks (id, created_at, full_text, screen_name, name, profile_image_url, url, favorite_count, retweet_count, views_count, archived, updated_at) VALUES 
-('1', NOW(), 'Just shipped a new feature 🚀 #coding #webdev', 'shadcn', 'shadcn', 'https://github.com/shadcn.png', 'https://twitter.com/shadcn/status/1', 120, 45, 1500, false, NOW()),
-('2', NOW(), 'Design systems are cool. Here is why.', 'sientio', 'Sientio', 'https://github.com/sientio.png', 'https://twitter.com/sientio/status/2', 89, 12, 800, false, NOW()),
-('3', NOW(), 'Rust is amazing for backend services.', 'rustlang', 'Rust', 'https://github.com/rust-lang.png', 'https://twitter.com/rustlang/status/3', 500, 200, 10000, false, NOW());
